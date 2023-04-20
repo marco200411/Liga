4a72d8ed5e45e34490694c62a7cd739984c885af
marco200411 <74360590+marco200411@users.noreply.github.com>
2023-04-20 13:40:20 +0200

    Day1

---
+++ Liga/src/Methods/Futbolista.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Methods;
+
+/**
+ *
+ * @author a22marcorr
+ */
+public class Futbolista extends Persona{
+    
+    
+    
+    
+    
+}
---
+++ Liga/src/Methods/Usuario.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Methods;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author a22marcorr
+ */
+public class Usuario {
+    String nombre;
+    String apellidos;
+    String nombreUsuario;
+    String contraseña;
+    ArrayList<Futbolista> jugadores = new ArrayList();
+    ArrayList<Futbolista> entrenadores = new ArrayList();
+    ArrayList<Futbolista> plantilla = new ArrayList(10);
+    Entrenador entrenadorPlantilla;
+    
+    
+    
+    
+    
+    
+}
